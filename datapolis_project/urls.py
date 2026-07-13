5548215627683f824b3028ceb12912a08bbcaf1d
Jean Pierre Londoño <jean.londono@anthesisgroup.com>
2026-07-13 18:20:44 -0500

    icono de google en el login

--- datapolis_project/urls.py
+++ datapolis_project/urls.py
@@ -20,10 +20,12 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("accounts/login/", RedirectView.as_view(pattern_name="login", permanent=False)),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("accounts/", include("allauth.urls")),
     path("", include("apps.platform.urls")),
